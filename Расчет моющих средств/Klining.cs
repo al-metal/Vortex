3bd1d15959671ee5ba58bcb356a5d22bfd44bf73
al-metal <al-metal@bk.ru>
2017-02-21 16:41:36 +0400

    Клининг сфера кухня

--- Расчет моющих средств/Klining.cs
+++ Расчет моющих средств/Klining.cs
@@ -80,6 +80,44 @@ namespace Расчет_моющих_средств
             "Для отбеливания и дезинфекции"
         };
 
+        string[] kuhPosudMashin = new string[2]
+        {
+            "Для мойки" ,
+            "Для ополаскивания"
+        };
+
+        string[] kuhHolodil = new string[2]
+        {
+            "Внутри" ,
+            "Снаружи"
+        };
+
+        string[] kuhVityajka = new string[2]
+        {
+            "Обезжиривание" ,
+            "Блеск"
+        };
+
+        string[] kuhMoika = new string[5]
+        {
+            "Стены, двери" ,
+            "Напольные покрытия" ,
+            "Окна и зеркала",
+            "Мусорные баки" ,
+            "Отбеливание мопов и полотенец"
+        };
+
+        string[] kuhTrub = new string[1]
+        {
+            "Устранение засоров"
+        };
+
+        string[] kuhRuki = new string[2]
+        {
+            "Жидкое мыло" ,
+            "Антисептик"
+        };
+
         #endregion
 
         #region Средства
@@ -87,6 +125,134 @@ namespace Расчет_моющих_средств
         {
             "Marvel" ,
             "Optima Gel"
+        };
+
+        string[] kuhPosudaGar = new string[1]
+        {
+            "Daze"
+        };
+
+        string[] kuhPosudaOtbel = new string[1]
+        {
+            "Fumigel"
+        };
+
+        string[] kuhPosudЬMashinMoika = new string[1]
+        {
+            "Blank"
+        };
+
+        string[] kuhPosudЬMashinOpolask = new string[1]
+        {
+            "DeBlank"
+        };
+
+        string[] kuhHolodilVnutr = new string[0]
+        {
+
+        };
+
+        string[] kuhHolodilsnaru = new string[3]
+        {
+            "Optima" ,
+            "Optima Gel",
+            "Well"
+        };
+
+        string[] kuhVityajkaObez = new string[2]
+        {
+            "Daze" ,
+            "Well"
+        };
+
+        string[] kuhVityajkaBlesk = new string[1]
+        {
+            "Twist"
+        };
+
+        string[] kuhPlita = new string[1]
+        {
+            "Daze"
+        };
+
+        string[] kuhSVCH = new string[2]
+        {
+            "Daze",
+            "Well"
+        };
+
+        string[] kuhPribor = new string[2]
+        {
+            "Marvel",
+            "Optima Gel"
+        };
+
+        string[] kuhMoechVanna = new string[1]
+        {
+            "Fumigel"
+        };
+
+        string[] kuhObDezinfec = new string[1]
+        {
+            "Fumigel"
+        };
+
+        string[] kuhRabStol = new string[2]
+        {
+            "Optima",
+            "Optima Gel"
+        };
+
+        string[] kuhMoikaSteny = new string[2]
+        {
+            "Optima" ,
+            "Optima Gel"
+        };
+
+        string[] kuhMoikaNapol = new string[2]
+        {
+            "Comfort" ,
+            "Comfort Extra"
+        };
+
+        string[] kuhMoikaOkna = new string[1]
+        {
+            "Magic"
+        };
+
+        string[] kuhMoikMusorBaki = new string[3]
+        {
+            "Optima",
+            "Optima Gel",
+            "Fumigel"
+        };
+
+        string[] kuhMoikOtbelMopov = new string[1]
+        {
+            "Fumigel"
+        };
+
+        string[] kuhTrubZasor = new string[1]
+        {
+            "Draft"
+        };
+
+        string[] kuhZapah = new string[2]
+        {
+            "Block",
+            "Fog"
+        };
+
+        string[] kuhRukiMylo = new string[3]
+        {
+            "Joy" ,
+            "Joy Platinum",
+            "Fay"
+        };
+
+        string[] kuhRukiAntysept = new string[1]
+        {
+            "Joy Sept"
         };
 
         #endregion
@@ -133,15 +299,8 @@ namespace Расчет_моющих_средств
             }
             else if (comboBox1.SelectedIndex == 5)
             {
-
+                combobox1item = 4;
             }
-        }
-
-        private void ItemsAddCB2(string[] array)
-        {
-            comboBox2.Items.Clear();
-            foreach (string str in array)
-                comboBox2.Items.Add(str);
         }
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
@@ -151,12 +310,58 @@ namespace Расчет_моющих_средств
             {
                 ItemsAddCB3(kuhPosuda);
             }
-        }
-
-        private void ItemsAddCB3(string[] array)
-        {
-            foreach (string str in array)
-                comboBox3.Items.Add(str);
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 1)
+            {
+                ItemsAddCB3(kuhPosudMashin);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 2)
+            {
+                ItemsAddCB3(kuhHolodil);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 3)
+            {
+                ItemsAddCB3(kuhVityajka);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 4)
+            {
+                ResultShowNoneCB3(kuhPlita);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 5)
+            {
+                ResultShowNoneCB3(kuhSVCH);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 6)
+            {
+                ResultShowNoneCB3(kuhPribor);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 7)
+            {
+                ResultShowNoneCB3(kuhMoechVanna);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 8)
+            {
+                ResultShowNoneCB3(kuhObDezinfec);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 9)
+            {
+                ResultShowNoneCB3(kuhRabStol);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 10)
+            {
+                ItemsAddCB3(kuhMoika);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 11)
+            {
+                ItemsAddCB3(kuhTrub);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 12)
+            {
+                ResultShowNoneCB3(kuhZapah);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 13)
+            {
+                ItemsAddCB3(kuhRuki);
+            }
         }
 
         private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
@@ -165,12 +370,102 @@ namespace Расчет_моющих_средств
             {
                 ResultShow(kuhPosudaZamach);
             }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 0 && comboBox3.SelectedIndex == 1)
+            {
+                ResultShow(kuhPosudaGar);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 0 && comboBox3.SelectedIndex == 2)
+            {
+                ResultShow(kuhPosudaOtbel);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 1 && comboBox3.SelectedIndex == 0)
+            {
+                ResultShow(kuhPosudЬMashinMoika);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 1 && comboBox3.SelectedIndex == 1)
+            {
+                ResultShow(kuhPosudЬMashinOpolask);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 2 && comboBox3.SelectedIndex == 0)
+            {
+                ResultShow(kuhHolodilVnutr);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 2 && comboBox3.SelectedIndex == 1)
+            {
+                ResultShow(kuhHolodilsnaru);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 3 && comboBox3.SelectedIndex == 0)
+            {
+                ResultShow(kuhVityajkaObez);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 3 && comboBox3.SelectedIndex == 1)
+            {
+                ResultShow(kuhVityajkaBlesk);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 10 && comboBox3.SelectedIndex == 0)
+            {
+                ResultShow(kuhMoikaSteny);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 10 && comboBox3.SelectedIndex == 1)
+            {
+                ResultShow(kuhMoikaNapol);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 10 && comboBox3.SelectedIndex == 2)
+            {
+                ResultShow(kuhMoikaOkna);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 10 && comboBox3.SelectedIndex == 3)
+            {
+                ResultShow(kuhMoikMusorBaki);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 10 && comboBox3.SelectedIndex == 4)
+            {
+                ResultShow(kuhMoikOtbelMopov);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 11 && comboBox3.SelectedIndex == 0)
+            {
+                ResultShow(kuhTrubZasor);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 13 && comboBox3.SelectedIndex == 0)
+            {
+                ResultShow(kuhRukiMylo);
+            }
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 13 && comboBox3.SelectedIndex == 1)
+            {
+                ResultShow(kuhRukiAntysept);
+            }
+        }
+
+
+        private void ItemsAddCB2(string[] array)
+        {
+            comboBox2.Items.Clear();
+            foreach (string str in array)
+                comboBox2.Items.Add(str);
+        }
+
+        private void ItemsAddCB3(string[] array)
+        {
+            comboBox3.Items.Clear();
+            foreach (string str in array)
+                comboBox3.Items.Add(str);
         }
 
         private void ResultShow(string[] array)
         {
+            textBox1.Clear();
+            foreach (string str in array)
+                textBox1.AppendText(str + "\n");
+        }
+
+        private void ResultShowNoneCB3(string[] array)
+        {
+            textBox1.Clear();
+            comboBox3.SelectedIndex = -1;
+            comboBox3.Items.Clear();
+            comboBox3.Enabled = false;
             foreach (string str in array)
                 textBox1.AppendText(str + "\n");
         }
     }
-    }
+}
